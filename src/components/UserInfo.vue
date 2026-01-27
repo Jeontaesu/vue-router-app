4ac9253756f719edd3d82025af5758fe524f886e
Jeontaesu <jeontaesu@gmail.com>
2026-01-27 23:54:31 +0900

    상태기반 UI조건 렌더링+라우터 연동실전

--- src/components/UserInfo.vue
+++ src/components/UserInfo.vue
@@ -1,9 +1,10 @@
 <template>
   <div v-if="store.username">
-    <p>
+    <p v-if="store.username">
       <strong>{{ store.username }}</strong
       >님, 환영합니다!
     </p>
+    <p v-else>이름을 입력해 주세요.</p>
   </div>
 </template>
 
--- src/main.js
+++ src/main.js
@@ -5,6 +5,6 @@ import router from "./router";
 
 const app = createApp(App);
 
-app.use(router); // Vue Router 등록
-app.use(createPinia()); // Pinia 등록
+app.use(router);
+app.use(createPinia());
 app.mount("#app");
